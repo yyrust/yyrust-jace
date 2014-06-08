68c7f8c6672e30f00842d15edfc0da052afb154e
yyrust <yyrust@localhost>
2014-06-08 14:45:08 -0400

    fix AutoProxy.sh for linux

--- bin/AutoProxy.sh
+++ bin/AutoProxy.sh
@@ -1,4 +1,5 @@
 export SCRIPT_PATH="$(readlink -f $(dirname "$0"))"
-export JACE_HOME=%SCRIPT_PATH%..
-export CLASSPATH="$(JACE_HOME)/core/java/target/*":"$(JACE_HOME)/core/java/target/dependencies/*":.
-java -classpath $(CLASSPATH) org.jace.proxy.AutoProxy $*
+export JACE_HOME=$SCRIPT_PATH/..
+export CLASSPATH="$JACE_HOME/core/java/target/*":"$JACE_HOME/core/java/target/dependency/*":.
+echo $CLASSPATH
+java -classpath $CLASSPATH org.jace.proxy.AutoProxy $*
